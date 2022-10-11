d31e180cc149a1972c2d79667c48c10123b006da
Douglas <douglasferreira@hdregistros.com.br>
2022-10-10 22:19:48 -0300

    Instalando Admin LTE

---
+++ src/app/tipo-descarte/tipo-descarte.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tipo-descarte',
+  templateUrl: './tipo-descarte.component.html',
+  styleUrls: ['./tipo-descarte.component.css']
+})
+export class TipoDescarteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

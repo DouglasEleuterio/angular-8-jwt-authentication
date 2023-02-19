2a18eae5df5fdb0009c45508c86a52b3aa030791
Douglas <douglas-negocios@hotmail.com>
2023-02-20 00:11:10 +0044

    Variavel de ambiente

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   // apiUrl: 'https://aterrosystem.herokuapp.com/api/',
-  // apiUrl: 'https://aterrosystem-migracao.herokuapp.com/api/',
-  apiUrl: 'http://localhost:8085/api/'
+  apiUrl: 'https://aterrosystem-migracao.herokuapp.com/api/',
+  // apiUrl: 'http://localhost:8085/api/'
 };
